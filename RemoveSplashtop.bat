7a20dde93d12e9f9f33c70e86cb212810107e3b5
Mongoose089 <47441524+Mongoose089@users.noreply.github.com>
2021-06-14 00:23:04 -0400

    Update RemoveSplashtop.bat

--- RemoveSplashtop.bat
+++ RemoveSplashtop.bat
@@ -15,5 +15,3 @@ sc delete "SSUService"
 
 RMDIR /S /Q "C:\ProgramData\Splashtop\Splashtop Software Updater" > nul 2> nul
 RMDIR /S /Q "C:\Program Files (x86)\Splashtop\Splashtop Software Updater" > nul 2> nul
-
-\\bluestonenas\BluestoneNASShare\Splashtop.exeCleanUpInstaller.exe
